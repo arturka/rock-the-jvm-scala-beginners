2bf285d2baacea18790cacb996c8bb16d55a18f8
arturka <a.abdalimov.97@gmail.com>
2019-01-02 11:59:04 +0600

    finished Section 3 Lecture 20

--- src/main/scala/lectures/part/oop/Generics.scala
+++ src/main/scala/lectures/part/oop/Generics.scala
@@ -7,6 +7,10 @@ object Generics extends App {
     def isEmpty: Boolean
     def head: A
     def tail: List[A]
+    def filter(predicate: MyPredicate[A]): List[A]
+    def map[B](transformer: MyTransformer[A, B]): List[B]
+    def flatMap[B](transformer: MyTransformer[A, List[B]]): List[B]
+    def ++[B >: A](list: List[B]): List[B]
   }
 
   object Empty extends List[Nothing] {
@@ -17,6 +21,14 @@ object Generics extends App {
     override def head: Nothing = throw new NoSuchElementException()
 
     override def tail: List[Nothing] = throw new NoSuchElementException()
+
+    override def filter(predicate: MyPredicate[Nothing]): List[Nothing] = Empty
+
+    override def map[B](transformer: MyTransformer[Nothing, B]): List[B] = Empty
+
+    override def flatMap[B](transformer: MyTransformer[Nothing, List[B]]): List[B] = Empty
+
+    override def ++[B >: Nothing](list: List[B]): List[B] = Empty
   }
 
   class GenericList[+A](elem: A, reminder: List[A]) extends List[A] {
@@ -27,6 +39,29 @@ object Generics extends App {
     override def head: A = elem
 
     override def tail: List[A] = reminder
+
+    override def filter(predicate: MyPredicate[A]): List[A] = {
+      if (predicate.test(elem)) new GenericList(elem, reminder.filter(predicate))
+      else reminder.filter(predicate)
+    }
+
+    override def map[B](transformer: MyTransformer[A, B]): List[B] = {
+      new GenericList(transformer.transform(elem), reminder.map(transformer))
+    }
+
+    def ++[B >: A](list: List[B]): List[B] = new GenericList[B](elem, reminder ++ list)
+
+    override def flatMap[B](transformer: MyTransformer[A, List[B]]): List[B] = {
+      transformer.transform(elem) ++ reminder.flatMap(transformer)
+    }
+  }
+
+  trait MyPredicate[-T] {
+    def test(elem: T): Boolean
+  }
+
+  trait MyTransformer[-A, B] {
+    def transform(elem: A): B
   }
 
 }
