6513f0e00045f9f7a38b482cc9086ebd5dbcec61
arturka <a.abdalimov.97@gmail.com>
2019-01-02 07:50:09 +0600

    finished Section 3 Lecture 17

---
+++ src/main/scala/exercises/MyList.scala
@@ -0,0 +1,30 @@
+package exercises
+
+abstract class MyList {
+
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(elem: Int): MyList
+  def toString(): String
+}
+
+object Empty extends MyList {
+  override def head: Int = throw new Exception("head of empty list")
+
+  override def tail: MyList = this
+
+  override def isEmpty: Boolean = true
+
+  override def add(elem: Int): MyList = new List(elem, Empty)
+}
+
+class List(elem: Int, reminder: MyList) extends MyList {
+  override def head: Int = elem
+
+  override def tail: MyList = reminder
+
+  override def isEmpty: Boolean = false
+
+  override def add(elem: Int): MyList = new List(elem, this)
+}
