51b76bc8820ccb45f3b7626abd4a05652cfa2bbb
arturka <a.abdalimov.97@gmail.com>
2019-01-04 00:06:40 +0600

    Deleted project files

--- core/src/main/scala/playgrond/JavaPlayground.java
+++
@@ -1,7 +0,0 @@
-package playgrond;
-
-public class JavaPlayground {
-    public static void main(String[] args) {
-        System.out.println("Hello, java");
-    }
-}
--- filesystem/src/main/scala/com/rtjvm/scala/oop/files/DirEntry.scala
+++
@@ -1,8 +0,0 @@
-package com.rtjvm.scala.oop.files
-
-abstract class DirEntry(val parentPath: String, val name: String) {
-
-  def path: String = s"$parentPath${Directory.Separator}$name"
-
-  def asDirectory: Directory
-}
